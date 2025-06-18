115456b8cb2868122e3c1100284a7d8b289ccc42
Preksha Koirala <prekshakoirala@gmail.com>
2025-06-18 14:22:22 -0500

    Initial Commit

---
+++ Seam/SeamApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeamApp.swift
+//  Seam
+//
+//  Created by Preksha Koirala on 6/18/25.
+//
+
+import SwiftUI
+
+@main
+struct SeamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
